87487a7b6534c47e393330a80d91a27b07fea380
sanyasimms <jain.sanya0210@gmail.com>
2025-10-29 09:47:31 +0530

    part-06

--- src/com/school/AttendanceRecord.java
+++ src/com/school/AttendanceRecord.java
@@ -1,37 +1,45 @@
 package com.school;
-
-public class AttendanceRecord {
+public class AttendanceRecord implements Storable {
     private int studentId;
     private int courseId;
     private String status;
 
+ 
     public AttendanceRecord(int studentId, int courseId, String status) {
         this.studentId = studentId;
         this.courseId = courseId;
-        
-        if ("Present".equalsIgnoreCase(status) || "Absent".equalsIgnoreCase(status)) {
-            this.status = status;
+        if (status.equalsIgnoreCase("Present") || status.equalsIgnoreCase("Absent")) {
+            this.status = status.substring(0, 1).toUpperCase() + status.substring(1).toLowerCase();
         } else {
             this.status = "Invalid";
-            System.out.println("Warning: Invalid attendance status provided. Set to 'Invalid'.");
+            System.out.println("⚠ Warning: Invalid attendance status entered. Must be 'Present' or 'Absent'.");
         }
     }
+  
 
+  
+    public int getStudentId() {
+        return studentId;
+    }
+
+  
+  public int getCourseId() {
+    return courseId;
+  }
    
-    public int getStudentId() { 
-        return studentId; 
-    }
 
-    public int getCourseId() { 
-        return courseId; 
-    }
-
-    public String getStatus() { 
-        return status; 
-    }
-
+  public String getStatus() {
+    return status;
+  }
     public void displayRecord() {
-        System.out.println("Attendance: Student ID " + studentId +
-                           " in Course ID C" + courseId + " - Status: " + status);
+        System.out.println("Student ID: " + studentId +
+                ", Course ID: " + courseId +
+                ", Status: " + status);
+    }
+
+
+    @Override
+    public String toDataString() {
+        return studentId + "," + courseId + "," + status;
     }
 }
